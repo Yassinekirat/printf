ff5b54a2787cc29c0164a3c417fa53cf231ccfac
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-13 20:29:37 -0700

    adding the core for my printf and fixing betty

---
+++ _printf_core.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf_core - the core of my printf function
+ * @format : format to be tested
+ * @args : arguments to be tested
+ * @printed_chars : characters to be printed
+ */
+void _printf_core(const char *format, va_list args, int *printed_chars)
+{
+	int i, d;
+	char c, *str;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_printf_nopercent(format, &i, printed_chars);
+			continue;
+		}
+		i++;
+		if (format[i] == '\0')
+			break;
+		if (format[i] == 'c')
+		{
+			c = va_arg(args, int);
+			_printf_char(c, printed_chars);
+		}
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			_printf_string(str, printed_chars);
+		}
+		else if (format[i] == 'd' || format[i] == 'i')
+		{
+			d = va_arg(args, int);
+			_printf_number(d);
+		}
+		else if (format[i] == '%')
+		{
+			_printf_percent(printed_chars);
+		}
+		else
+			_printf_random(format, printed_chars, i);
+	}
+}
